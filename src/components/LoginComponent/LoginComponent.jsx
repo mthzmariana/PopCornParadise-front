c45ee86fe5e288fc9e5f5b91f7a8861665b15a62
YazminMedina <yesseniamedina35@gmail.com>
2024-06-14 14:35:28 -0600

    Login y Registro Vista

--- src/components/LoginComponent/LoginComponent.jsx
+++ src/components/LoginComponent/LoginComponent.jsx
@@ -1,10 +1,37 @@
-import "./LoginComponent.css";
+import React from 'react';
+import './LoginComponent.css';
+import LogoSinFondo from "../../assets/LogoSinFondo.png";
 
 function LoginComponent() {
-
   return (
-    <div>
-        <h1>Iniciar sesión</h1>
+    <div className="login-page">
+      
+      <div className="formL">
+        
+        <form>
+        <div className="img-sm">
+          <img src={LogoSinFondo} alt="LogoL"/>
+        </div>
+          <div>
+            <input
+              className="input-box"
+              type="email"
+              placeholder="Correo"
+            />
+          </div>
+          <div>
+            <input
+              className="input-box"
+              type="password"
+              placeholder="Contrasena"
+            />
+          </div>
+          <button type="button">Login</button>
+          <br />
+          <br />
+          <p className="text-black">¿No tienes cuenta? <a href="/registro">Regístrate aquí</a></p>
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/RegistroComponent/RegistroComponent.jsx
+++ src/components/RegistroComponent/RegistroComponent.jsx
@@ -1,10 +1,52 @@
+import React from 'react';
 import "./RegistroComponent.css";
+import LogoSinFondo from "../../assets/LogoSinFondo.png";
 
 function RegistroComponent() {
-
   return (
-    <div>
-        <h1>Registro</h1>
+    <div className="login-page">
+      <div className="formR">
+        
+        <form>
+        <div className="img-sm">
+          <img src={LogoSinFondo} alt="LogoL"/>
+        </div>
+          <div>
+            <input
+              type="text"
+              name="inputNombre"
+              id="inputNombre"
+              placeholder="Usuario"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="inputCorreo"
+              id="inputCorreo"
+              placeholder="Correo"
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              name="inputEdad"
+              id="inputEdad"
+              placeholder="Edad"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="inputPassworld"
+              id="inputPassworld"
+              placeholder="Contrasena"
+            />
+          </div>
+          <button type="button">Crear</button>
+          <p className="message">¿Ya estás registrado? <a href="/login">Inicia sesión</a></p>
+        </form>
+      </div>
     </div>
   );
 }
