a8de5e48be77e0f79a0caca061afcdcc8bbe6930
Mariana Martinez <martinezhz.mariana@gmail.com>
2024-06-15 20:29:42 -0600

    Navbar ajustado

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -5,20 +5,32 @@ function NavbarComponent() {
   return (
     <div>
       <nav className="navbar">
-        <button className="MiniLogo">
-          <a href="/">
-            <MiniLogo />
-          </a>
+        <div className="nav-links-container">
+          <button className="MiniLogo">
+            <a href="/">
+              <MiniLogo />
+            </a>
+          </button>
+          <div className="nav-links">
+            <a href="/peliculas">Películas</a>
+            <a href="/acerca-de">Acerca de</a>
+            <a href="/contacto">Contacto</a>
+          </div>
+          <div className="auth-links">
+            <a href="/login">Iniciar sesión</a>
+            <a href="/registro">Registrarse</a>
+          </div>
+        </div>
+        <button
+          className="mobile-menu-icon"
+          onClick={() => {
+            document
+              .querySelector(".nav-links-container")
+              .classList.toggle("active");
+          }}
+        >
+          ☰
         </button>
-        <div className="nav-links">
-          <a href="/peliculas">Películas</a>
-          <a href="/acerca-de">Acerca de</a>
-          <a href="/contacto">Contacto</a>
-        </div>
-        <div className="nav-links">
-          <a href="/login">Iniciar sesión</a>
-          <a href="/registro">Registrarse</a>
-        </div>
       </nav>
     </div>
   );
