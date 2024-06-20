c8f94a52b3b4a6669e7b7eabbd20c09b46747cee
Mariana Martinez <martinezhz.mariana@gmail.com>
2024-06-20 04:58:22 -0600

    Creación de vista Acerca de. Y ajustes en otros archivos

--- src/components/AcercaComponent/AcercaComponent.jsx
+++ src/components/AcercaComponent/AcercaComponent.jsx
@@ -1,10 +1,47 @@
 import "./AcercaComponent.css";
+import LogoNaranaja from "../../icons/logonaranja.jsx";
 
 function AcercaComponent() {
 
   return (
     <div>
-        <h1>Acerca de</h1>
+        <section className="cont-acerca">
+          <div className="titulo-acerca">
+            <h1>Sobre Popcorn Paradise</h1>
+          </div>
+
+          <div className="cont-info">
+
+            <div className="cuadro-info">
+              <div>
+                <h2>Nuestra historia</h2>
+              </div>
+              <div>
+                <p>Hemos sido un referente en la industria cinematográfica, comenzando con la venta de películas en formato físico y evolucionando para ofrecer nuestros servicios digitalmente.</p>
+              </div>
+            </div>
+
+            <div className="cuadro-info">
+              <div>
+                <h2>Qué hacemos</h2>
+              </div>
+              <div>
+                <p>Ofrecemos un catálogo de películas para compra, accesible desde cualquier dispositivo con conexión a internet de forma segura y fácil de usar.</p>
+              </div>
+            </div>
+
+            <div className="cuadro-info">
+              <div>
+                <h2>Nos importas</h2>
+              </div>
+              <div>
+                <p>Nos esforzamos por proporcionar un servicio excepcional. Queremos que cada visita a Popcorn Paradise sea una experiencia positiva y memorable.</p>
+              </div>
+            </div>
+
+          </div>
+          <LogoNaranaja />
+        </section>
     </div>
   );
 }
