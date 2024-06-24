b036e6d48a1d13f868e7a684d987418eb7bf0ec5
Mariana Martinez <martinezhz.mariana@gmail.com>
2024-06-23 18:00:15 -0600

    Vista de contacto realizada

--- src/components/ContactoComponent/ContactoComponent.jsx
+++ src/components/ContactoComponent/ContactoComponent.jsx
@@ -1,65 +1,82 @@
-import React, { useState } from 'react';
-import './ContactoComponent.css';
-import ClaquetaLogo from '../../assets/Claqueta.png';
+import React, { useState } from "react";
+import "./ContactoComponent.css";
+import Claqueta from "../../assets/Claqueta.png";
+import LogoNaranaja from "../../icons/logonaranja.jsx";
 
 const ContactoComponent = () => {
-  const [nombre, setNombre] = useState('');
-  const [email, setEmail] = useState('');
-  const [comentarios, setComentarios] = useState('');
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [comentarios, setComentarios] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Manejar el envío del formulario aquí
-    console.log('Nombre:', nombre);
-    console.log('Email:', email);
-    console.log('Comentarios:', comentarios);
+    console.log("Nombre:", nombre);
+    console.log("Email:", email);
+    console.log("Comentarios:", comentarios);
   };
 
   return (
-    <div className="contact-page">
-      <div className="logo-container">
-        <img src={ClaquetaLogo} alt="Claqueta Logo" />
-      </div>
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="nombre">Nombre</label>
-            <input
-              id="nombre"
-              className="input-box"
-              type="text"
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
-              placeholder="Escribe tu nombre"
-              required
-            />
+    <div>
+      <section className="contact-page">
+        <div className="cont-img-form">
+          <div className="elemento-form">
+            <img className="claqueta" src={Claqueta} alt="Claqueta" />
           </div>
-          <div className="form-group">
-            <label htmlFor="email">Correo</label>
-            <input
-              id="email"
-              className="input-box"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="correo@ejemplo.com"
-              required
-            />
+
+          <div className="elemento-form">
+            <form className="cont-formulario" onSubmit={handleSubmit}>
+              <div>
+                <label className="label-form" htmlFor="nombre">
+                  Nombre
+                </label>
+                <input
+                  id="nombre"
+                  className="input-form"
+                  type="text"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
+                  placeholder="Escribe tu nombre"
+                  required
+                />
+              </div>
+              <div>
+                <label className="label-form" htmlFor="email">
+                  Correo
+                </label>
+                <input
+                  id="email"
+                  className="input-form"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="correo@ejemplo.com"
+                  required
+                />
+              </div>
+              <div>
+                <label className="label-form" htmlFor="comentarios">
+                  Comentarios
+                </label>
+                <textarea
+                  id="comentarios"
+                  className="input-form"
+                  value={comentarios}
+                  onChange={(e) => setComentarios(e.target.value)}
+                  placeholder="Escribe tus comentarios aquí"
+                  rows="4"
+                  required
+                />
+              </div>
+              <button className="button-env" type="submit">
+                Enviar
+              </button>
+            </form>
           </div>
-          <div className="form-group">
-            <label htmlFor="comentarios">Comentarios</label>
-            <textarea
-              id="comentarios"
-              className="input-box"
-              value={comentarios}
-              onChange={(e) => setComentarios(e.target.value)}
-              placeholder="Escribe tus comentarios aquí"
-              rows="4"
-              required
-            />
-          </div>
-          <button type="submit">Enviar</button>
-        </form>
+        </div>
+      </section>
+      <div className="logo-naranja">
+        <LogoNaranaja />
       </div>
     </div>
   );
