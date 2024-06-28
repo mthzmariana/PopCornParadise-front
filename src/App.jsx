49747a609fae87f2942da5a133f9174cccc290b7
Yaneli Noemi Cel Couoh <yanelicelcouoh@gmail.com>
2024-06-28 11:41:25 -0600

    Resuelve conflictos entre pp-perfil y main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 import NavbarComponent from "./components/NavbarComponent/NavbarComponent";
 import HomeComponent from "./components/HomeComponent/HomeComponent";
 import AcercaComponent from "./components/AcercaComponent/AcercaComponent";
@@ -8,26 +9,30 @@ import RegistroComponent from "./components/RegistroComponent/RegistroComponent"
 import LoginComponent from "./components/LoginComponent/LoginComponent";
 import PeliculasComponent from "./components/PeliculasComponent/PeliculasComponent";
 import PerfilComponent from "./components/PerfilComponent/PerfilComponent";
-import EditUserComponent from "./components/EditUserComponent/EditUserComponent";
+import EditUserComponent from "./components/EditUserComponent/EditUserComponent"; // Importa EditUserComponent
+import FooterComponent from "./components/FooterComponent/FooterComponent"; // Importa FooterComponent
 import { UserProvider } from './contexts/UserContext'; // Importa el UserProvider
 
 function App() {
+  const [footerFlag, setFooterFlag] = useState(true);
+  const [navbarFlag, setNavbarFlag] = useState(true);
+
   return (
     <UserProvider>
       <Router>
         <div>
-          <NavbarComponent />
+          {navbarFlag && <NavbarComponent />}
           <Routes>
             <Route exact path="/" element={<HomeComponent />} />
             <Route exact path="/acerca-de" element={<AcercaComponent />} />
             <Route exact path="/contacto" element={<ContactoComponent />} />
-            <Route exact path="/registro" element={<RegistroComponent />} />
-            <Route exact path="/login" element={<LoginComponent />} />
+            <Route exact path="/registro" element={<RegistroComponent handleNavbar={setNavbarFlag} handleFooter={setFooterFlag} />} />
+            <Route exact path="/login" element={<LoginComponent handleNavbar={setNavbarFlag} handleFooter={setFooterFlag} />} />
             <Route exact path="/peliculas" element={<PeliculasComponent />} />
             <Route exact path="/perfil" element={<PerfilComponent />} />
             <Route exact path="/editar-perfil/:id" element={<EditUserComponent />} />
-
-            </Routes>
+          </Routes>
+          {footerFlag && <FooterComponent />}
         </div>
       </Router>
     </UserProvider>
